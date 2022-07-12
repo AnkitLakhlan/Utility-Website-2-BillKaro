fd781181657dbf0d5973ccd314f2c0a206e8b649
unknown <ankitlakhlan03@gmail.com>
2022-07-12 22:35:24 +0530

    payment pages created/ orderhistory page remaining

--- js/js-dist.js
+++ js/js-dist.js
@@ -1,4 +1,5 @@
-if (function(e, t) {
+
+  if (function(e, t) {
         "object" == typeof module && "object" == typeof module.exports ? module.exports = e.document ? t(e, !0) : function(e) {
             if (!e.document) throw new Error("jQuery requires a window with a document");
             return t(e)
